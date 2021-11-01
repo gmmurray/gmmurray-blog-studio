34711c65489e2641618df9a27a5d0f28b14d36bb
Greg Murray <44830296+gmmurray@users.noreply.github.com>
2021-10-31 23:31:32 -0400

    initial commit

---
+++ schemas/post.js
@@ -0,0 +1,67 @@
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "mainImageSrc",
+      title: "Main image source",
+      type: "string",
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "reference",
+      to: [{ type: "category" }],
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      type: "tags",
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+    },
+    {
+      name: "summary",
+      title: "Summary",
+      type: "string",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "mainImage",
+    },
+  },
+};
